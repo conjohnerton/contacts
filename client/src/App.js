8e41f649c6c14d503212bf0340d70604a74847b4
John C <conjohnerton@gmail.com>
2019-09-09 23:22:19 -0400

    removed less from theming

--- client/src/App.js
+++ client/src/App.js
@@ -17,66 +17,11 @@ import {
 import LoginForm from "./components/LoginForm";
 import "./styles/App.css";
 
-class MenuExampleVertical extends Component {
-	state = { activeItem: "inbox" };
-
-	handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
-	render() {
-		const { activeItem } = this.state;
-
-		return (
-			<Menu vertical>
-				<Menu.Item
-					name="inbox"
-					active={activeItem === "inbox"}
-					onClick={this.handleItemClick}
-				>
-					<Label color="teal">1</Label>
-					Inbox
-				</Menu.Item>
-
-				<Menu.Item
-					name="spam"
-					active={activeItem === "spam"}
-					onClick={this.handleItemClick}
-				>
-					<Label>51</Label>
-					Spam
-				</Menu.Item>
-
-				<Menu.Item
-					name="updates"
-					active={activeItem === "updates"}
-					onClick={this.handleItemClick}
-				>
-					<Label>1</Label>
-					Updates
-				</Menu.Item>
-				<Menu.Item>
-					<Input icon="search" placeholder="Search mail..." />
-				</Menu.Item>
-			</Menu>
-		);
-	}
-}
-
 function App() {
 	const [visible, setVisible] = useState(false);
 	const [activePage, setActivePage] = useState("home");
 
-	return (
-		<div className="App">
-			<Container>
-				<MenuExampleVertical />
-				<Menu secondary>
-					<Menu.Item name="home" onClick={() => "a"} />
-					<Menu.Item />
-					<Menu.Item />
-				</Menu>
-			</Container>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import "semantic-ui-less/semantic.less";
+import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
