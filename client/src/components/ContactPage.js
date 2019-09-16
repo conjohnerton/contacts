d1798ff7e5db731db10b8d2c7762acf5bd7bafec
DreamsInCS <46572490+DreamsInCS@users.noreply.github.com>
2019-09-16 19:51:35 -0400

    I'm {ahead on my way} to expanding the contact page!

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import {
 	Button,
 	Container,
@@ -14,6 +15,66 @@ import {
 	Visibility
 } from "semantic-ui-react";
 
+const doNothing = () => {
+	console.log("Hey, you did it!");
+};
+
+const ContactPageSidebar = () => {
+	const 
+
+	<SidebarPushable>
+		<Sidebar as={Menu}>
+			<MenuItem>
+				<form>
+					<input
+					value={}
+					onChange={}
+					/>
+				</form>
+			</MenuItem>
+			<MenuItem>
+			</MenuItem>
+			<MenuItem>
+			</MenuItem>
+		</Sidebar>
+	</SidebarPushable>
+};
+
+const HomepageHeading = () => (
+	<Container text>
+		<Header
+			as="h1"
+			color="grey"
+			content="Where Are My Contacts?"
+			inverted
+			style={{
+				fontSize: "4em",
+				fontWeight: "normal",
+				marginBottom: 0,
+				marginTop: "3em"
+			}}
+		/>
+		<Header
+			as="h2"
+			color="grey"
+			content="A contact manager... for your grandma!"
+			inverted
+			style={{
+				fontSize: "1.7em",
+				fontWeight: "normal",
+				marginTop: "1.5em"
+			}}
+		/>
+
+		<Link to="/signup">
+			<Button primary size="huge">
+				Get Started
+				<Icon name="right arrow" />
+			</Button>
+		</Link>
+	</Container>
+);
+
 const ContactPage = () => {
     const ContactPageHeading = () => (
         <Container text>
@@ -24,6 +85,7 @@ const ContactPage = () => {
     return (
 		<Container>
 			<ContactPageHeading />
+			<HomepageHeading />
 		</Container>
 	);
 };
