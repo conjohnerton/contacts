37dcb4d8331e6134bc96d022c5060c128bfe75d8
John C <conjohnerton@gmail.com>
2019-09-27 13:12:42 -0400

    added heroku postbuild

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ function auth(req, res, next) {
 
 	try {
 		// Verify token
-		const decoded = await jwt.verify(token, config.get("jwtSecret"));
+		const decoded = jwt.verify(token, config.get("jwtSecret"));
 
 		// Add user from payload
 		req.user = decoded;
